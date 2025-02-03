5670ca6ea07a3d4db66e3e4576baf6f83fbde404
Pranav-Pote-28 <129758900+Pranav-Pote-28@users.noreply.github.com>
2025-02-03 17:13:10 +0530

    commit_17_13

--- script.js
+++ script.js
@@ -6,28 +6,34 @@ function toggleMenu(){
 }
 
 var moonIcon=document.getElementById("moon-icon");
+var moonIconMedia=document.getElementById("moon-icon-media");
+
 var linkedinIcon=document.getElementById("linkedin-icon");
 var linkedinIcon2=document.getElementById("linkedin-icon-2")
 var githubIcon=document.getElementById("github-icon");
 var mailIcon=document.getElementById("mail-icon");
 var gradIcon=document.getElementById("grad-cap");
 
-moonIcon.onclick=function (){
+
+
+function toggleDarkMode(){
+    
+    console.log("clicked")
   
     document.body.classList.toggle("dark-theme");
-
+    
     if(document.body.classList.contains("dark-theme")){
         moonIcon.src="./assets/sun.png";
         moonIcon.title="light mode";
-
+        
         linkedinIcon.src="./assets/linkedin_dark.png";
         linkedinIcon2.src="./assets/linkedin_dark.png";
         githubIcon.src="./assets/github_dark.png";
         mailIcon.src="./assets/mail_dark.png";
         gradIcon.src="./assets/grad-dark.png";
-
-
-
+        
+        
+        
     }else{
         moonIcon.src="./assets/moon.png";
         linkedinIcon.src="./assets/linkedin.png";
@@ -35,6 +41,10 @@ moonIcon.onclick=function (){
         githubIcon.src="./assets/github.png";
         mailIcon.src="./assets/email.png";
         gradIcon.src="./assets/education.png";
-
+        
     }
 }
+
+
+moonIcon.onclick=toggleDarkMode;
+moonIconMedia.onclick=toggleDarkMode;
